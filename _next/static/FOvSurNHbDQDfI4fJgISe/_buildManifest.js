0f1a0ce112c163703d8dde46b47d2e1fa50c4205
ak1t0 <ak1t0@users.noreply.github.com>
2021-01-24 07:08:51 +0000

    deploy: a08e989cfed745868333ef0818135edc1bd54b4c

---
+++ _next/static/FOvSurNHbDQDfI4fJgISe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fa9a7754c.8092292352bc3d20a0ef.js","static\u002Fchunks\u002Fpages\u002Findex-2fcd801735106197a197.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b2e8d81a7624e665d650.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
